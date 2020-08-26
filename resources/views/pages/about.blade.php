5eff2fbaac16fc6746b0970aef4313c572de2371
jmumo <johnmumo22@gmail.com>
2020-08-26 17:00:34 +0300

    my first commit

--- resources/views/pages/about.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
- @section('content')
-     <h2> {{$title}} </h2>
-     <p>{{$services}}</p>
-     <p>{{$name}}</p>
-     @endsection
--- resources/views/pages/index.blade.php
+++
@@ -1,9 +0,0 @@
-
-@extends('layouts.app')
-@section('content')
-<h1> welcome to mumo dev</h1>
-{{--<h3>{{$brand}}</h3>--}}
-<p>this is my first laravel trial</p>
-
-
-    @endsection
